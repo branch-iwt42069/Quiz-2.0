0ee2bea0436f8e51a571166e837baa0523d864e2
Egor <steppunch@mail.ru>
2020-05-05 16:51:10 +0700

    Programming MORE

--- bin/3.cpp
+++ bin/3.cpp
@@ -36,4 +36,23 @@ cin.get();
 string Name;
 int Age;
 
+cout <<"What is your name?" << endl;
+cin >> Name;
+cout << endl;
+cout <<"How old are you?" << endl;
+cin >> Age;
+cout << endl;
+string Respond;
+cout << "Are you ready to take the quiz " << Name << "? Yes/no" << endl;
+cin >> Respond;
+if (Respond == "yes")
+{
+	cout << endl;
+	cout << "ok, goodluck!" << endl;
+}
+else {
+	cout << "ok, goodbye!" << endl;
+	return 0;
+}
+
 
