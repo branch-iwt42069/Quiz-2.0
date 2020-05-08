803bf8908c633461eaa2b6104d54bfac5b548faa
kontra0061 <kontra0061@gmail.com>
2020-05-08 19:32:16 +0700

    Fix program

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@ int main()
     cout << "*Title here*" << endl;
     cout << "* * * * * *" << endl; // making menu
     
-    cout << "MADE BY NOSKOV SERGEY, STEPANCHENKO EGOR AND MATASHEV VLADIMIR"  
-	<<endl;
 
     cout << "Press Enter to start the quiz .. " << endl;
     cin.get();
